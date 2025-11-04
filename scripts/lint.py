0b07790768995fe5949e4e084160043fe4d2f11d
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-11-04 14:07:10 -0500

    add ignore directory

--- scripts/lint.py
+++ scripts/lint.py
@@ -7,6 +7,13 @@ if len(sys.argv) != 2:
     print(f"Usage: {sys.argv[0]} <directory-to-scan>")
     sys.exit(2)
 
+IGNORE_DIRS = {'.git', '__pycache__'}
+
+def should_skip(path: str) -> bool:
+    parts = path.split('/')
+    return any(part in IGNORE_DIRS for part in parts)
+
+
 start_dir = sys.argv[1]
 if not os.path.isdir(start_dir):
     print(f"Error: '{start_dir}' is not a directory or does not exist.")
@@ -19,15 +26,22 @@ emoticon_pattern = regex.compile(r'[:;=8][\-~^]?[)(DPp]')
 found = False
 
 for root, dirs, files in os.walk(start_dir):
+    # skip directories
+    if should_skip(root):
+        continue
+
     for name in files:
         path = os.path.join(root, name)
+        if should_skip(path):
+            continue
+
         rel = os.path.relpath(path, start_dir)
         
         if cyr_pattern.search(name):
             print(f"::error file={rel}::filename contains Cyrillic")
             found = True
 
-        # Check content
+        # check file content
         try:
             with open(path, encoding='utf-8', errors='ignore') as f:
                 for num, line in enumerate(f, 1):
@@ -45,5 +59,3 @@ if found:
 else:
     print("::notice:: No Cyrillic or emoji found")
     sys.exit(0)
-
-
